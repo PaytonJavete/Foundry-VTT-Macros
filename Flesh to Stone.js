0487ad17a2ff2cf18332aa6c5263bb9dc86a6b86
PaytonJavete <41489644+PaytonJavete@users.noreply.github.com>
2023-03-24 12:11:38 -0700

    Remove Floating Files

--- Flesh to Stone.js
+++
@@ -1,9 +0,0 @@
-lastArg = args[args.length-1];
-if (lastArg.failedSaves.length > 0){
-	let tracker = {"fails": 1, "saves": 0};
-	for (token of lastArg.failedSaves){
-		let c_token = canvas.tokens.get(token.id);
-		let actor = c_token.actor;
-		DAE.setFlag(actor, "FleshToStone", tracker);
-	}
-}
