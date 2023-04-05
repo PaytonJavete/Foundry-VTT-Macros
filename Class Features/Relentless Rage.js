523f21b74707216915d6f296d5b7e9c2d51269c6
PaytonJavete <41489644+PaytonJavete@users.noreply.github.com>
2023-04-04 20:21:19 -0700

    Relentless Rage Player Rolls

--- Class Features/Relentless Rage.js
+++ Class Features/Relentless Rage.js
@@ -1,10 +1,9 @@
 async function wait(ms) { return new Promise(resolve => { setTimeout(resolve, ms); }); }
 const lastArg = args[args.length - 1];
-let actor = canvas.tokens.get(lastArg.tokenId).actor;
-let DC = args[1];
-hasEffectApplied = await game.dfreds.effectInterface.hasEffectApplied('Rage', actor.uuid);
-effectData = lastArg.efData;
-console.log(args);
+const actor = canvas.tokens.get(lastArg.tokenId).actor;
+const DC = args[1];
+const hasEffectApplied = await game.dfreds.effectInterface.hasEffectApplied('Rage', actor.uuid);
+let effectData = lastArg.efData;
 
 if (args[0] == "off" && lastArg["expiry-reason"] == 'midi-qol:rest') {	
 	effectData.changes[0].value = 10;
@@ -13,8 +12,22 @@ if (args[0] == "off" && lastArg["expiry-reason"] == 'midi-qol:rest') {
 
 if (args[0] == "off" && lastArg["expiry-reason"] == 'midi-qol:zeroHP' && hasEffectApplied) {
 	await wait(1000); // needed cause sometiems updates too fast
-	save_roll = await actor.rollAbilitySave('con', {chatMessage : true, async: true });
-	if (save_roll.total >= DC){
+
+	const item = await actor.items.getName("Relentless Rage");
+	const workflowItemData = duplicate(item);
+	workflowItemData.system.save.dc == DC;
+
+	setProperty(workflowItemData, "flags.itemacro", {});
+    setProperty(workflowItemData, "flags.midi-qol", {});
+    setProperty(workflowItemData, "flags.dae", {});
+    setProperty(workflowItemData, "effects", []);
+    delete workflowItemData._id;
+
+	const spellItem = new CONFIG.Item.documentClass(workflowItemData, { parent: actor });
+    const options = { showFullCard: false, createWorkflow: true, configureDialog: false };
+    const result = await MidiQOL.completeItemRoll(spellItem, options);
+
+	if (result.failedSaves.size == 0){
 		await actor.update({"system.attributes.hp.value": 1});
 		effectData.changes[0].value = DC+5;
 	}
