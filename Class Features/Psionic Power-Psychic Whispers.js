e48e283362aeafd518226bec5a925cea95a19b5f
PaytonJavete <41489644+PaytonJavete@users.noreply.github.com>
2023-04-24 22:38:10 -0700

    add robe of stars

--- Class Features/Psionic Power-Psychic Whispers.js
+++ Class Features/Psionic Power-Psychic Whispers.js
@@ -1,7 +1,7 @@
 const lastArg = args[args.length - 1];
 
 duration = 3600 * lastArg.damageTotal;
-const effectData = game.dfreds.effectInterface.findEffectByName('Psychic Whispers').convertToObject();
-effectData.seconds = duration;
+effectData = game.dfreds.effectInterface.findEffectByName('Psychic Whispers').data;
+effectData.duration.seconds = duration;
 
-lastArg.targetUuids.forEach(uuid => game.dfreds.effectInterface.addEffectWith({ effectData, uuid }));	
+lastArg.targetUuids.forEach(uuid => game.dfreds.effectInterface.addEffectWith({ effectData, uuid }));
