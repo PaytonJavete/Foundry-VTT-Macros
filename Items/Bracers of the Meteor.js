a5cf05cf51c6683bc9ca3e556585e3dbef243259
PaytonJavete <41489644+PaytonJavete@users.noreply.github.com>
2023-05-21 15:10:30 -0700

    Update Creed

---
+++ Items/Bracers of the Meteor.js
@@ -0,0 +1,12 @@
+const lastArg = args[args.length - 1];
+const target = await fromUuid(lastArg.tokenUuid);
+
+new Sequence()
+	.effect()
+	  .file("modules/jb2a_patreon/Library/Cantrip/Toll_The_Dead/TollTheDeadShockwave_01_Regular_Blue_400x400.webm")
+	  .size({
+	    width: canvas.grid.size * (25 / canvas.dimensions.distance),
+	    height: canvas.grid.size * (25 / canvas.dimensions.distance),
+	  })
+	  .atLocation(target)
+	.play();
